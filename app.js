a9887d99b1cdefaf462a5ee5ae3cb7d1818e21ad
Lewis Hellewell <lewis@Lewiss-MacBook-Air.local>
2022-04-19 09:58:55 +0800

    implemented server side word target

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const targetarray = ["plane", "turbo", "brawn", "teddy", "train", "power"];
-const target = targetarray[Math.floor(Math.random() * targetarray.length)];
+//const target = targetarray[Math.floor(Math.random() * targetarray.length)];
 let guesscount = 0;
 
 window.onload = function () {
@@ -15,40 +15,39 @@ window.onload = function () {
         }
         table.appendChild(tablerow);
     }
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        console.log(this.responseText);
-    };
-    xhttp.open("GET", "https://drtnf.net/wordle_guess?guess=GUESS");
-    xhttp.send();
+    
+    
 }
 
-function wordle(guess) {
-    
-    var output = [];
-    var i = 0;
-    let targetclone = target;
+$(document).ready(function() {
+    $("button").click(function() {
+        
+        let guess = document.getElementById("guess").value.toLowerCase();
+
+        $.get("https://drtnf.net/wordle_guess?guess=" + guess, function(data) {
+            guessOutcome(data.outcome);
+        }) 
+
+    });
+});
+
+function guessOutcome(outcome) {
     let correctcount = 0;
-    
-    for (var char of guess) {
-        document.getElementById(guesscount*10+i+1).innerHTML = char.toUpperCase();
-        if (char === target[i])  {
-            output[i] = 2;
+
+    let guess = document.getElementById('guess').value
+    for (let i = 0; i < outcome.length; i++) {
+        document.getElementById(guesscount*10+i+1).innerHTML = guess[i].toUpperCase();
+        if (outcome[i] == 2)  {
             document.getElementById(guesscount*10+i+1).style.backgroundColor = 'green';
             correctcount++;
         }
-        else if (targetclone.includes(char))    {
-            targetclone = targetclone.slice(0, targetclone.indexOf(char)) + targetclone.slice(targetclone.indexOf(char)+1, target.length);
-            output[i] = 1;
+        else if (outcome[i] == 1)    {
             document.getElementById(guesscount*10+i+1).style.backgroundColor = 'yellow';
         } else {
-            output[i] = 0;
             document.getElementById(guesscount*10+i+1).style.backgroundColor = 'gray';
         }
-        i++;
+        
     }
-    document.getElementById("wordle").innerHTML = output;
     guesscount++;
     if (correctcount == 5)  {
         document.getElementById("submit").style.display = "none";
@@ -60,5 +59,6 @@ function wordle(guess) {
         document.getElementById("wordle").innerHTML = "RAN OUT OF GUESSES";
         document.getElementById("wordle").style.color = "red";
     }
+    
     document.getElementById('guess').value = '';
 }
